63c9ffb9e1992a2a1677dc5fa8bb823c48df38e2
Sean Kelley <sk@Mac.lan>
2026-03-18 19:41:54 -0500

    First init

---
+++ header.php
@@ -0,0 +1,41 @@
+<header class="sticky-header">
+	<div class="sitename">
+		Faux Facts
+	</div>
+
+	<!-- Logo -->
+	<a href="index.php"><img src="Images/Fox Logo.svg" alt="Logo"></a>
+
+	<ul id="menu"> 
+		<li>
+			<div class="dropdown">
+				<button class="dropbtn">About</button>
+				<div class="dropdown-content">
+					<a href="about.php">About us</a>
+					<a href="stand.php">What we stand for</a>
+					<a href="help.php">How you can help</a>
+				</div>
+			</div>
+		</li>
+
+		<li>
+			<div class="dropdown">
+				<button class="dropbtn">Resources</button>
+				<div class="dropdown-content">
+					<a href="#">Blank Page</a>
+					<a href="other-orgs.php">Other Organizations</a>
+				</div>
+			</div>
+		</li>
+
+		<li>
+			<div class="dropdown">
+				<button class="dropbtn">Activisim</button>
+				<div class="dropdown-content">
+					<a href="donate.php">Donations</a>
+					<a href="laws.php">Laws</a>
+				</div>
+			</div>
+		</li>
+	</ul>
+</header>
